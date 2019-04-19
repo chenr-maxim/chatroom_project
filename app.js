cd2ca3eda21b924ec1f929582f7b937142bfda15
Max Chen <chenr.maxim@gmail.com>
2019-04-18 17:11:06 -0700

    got send message to work

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
+var ROOM_ID = '19727781';
+var currentUser = ''
+
 window.onload = function(e) {
     const chatManager = hookUpChatKit();
-    const ROOM_ID = '19707012';
-
-    chatManager
-    .connect()
-        .then(currentUser => {
-        // console.log(currentUser.rooms);
-        // console.log(currentUser.roomSubscriptions);
-        currentUser.subscribeToRoomMultipart({
-        roomId: currentUser.rooms[0].id,
-        hooks: {
-            onMessage: message => {
-            console.log("Received message:", message.parts[0].payload.content)
-            }
-        },
-        messageLimit: 0
-        }).then(response => {
-            fetchMessages(currentUser);
-        })
-        .catch(err => {
-            console.log("error");
-            console.log(err)
-        })
-    });
-
-    function fetchMessages(currentUser) {
-        currentUser.fetchMultipartMessages({
-            roomId: ROOM_ID,
-            direction: 'older',
-            limit: 10,
-        })
-        .then(messages => {
-            for(var i = 0; i < messages.length; i++) {
-                var node = document.createElement("LI")
-                var writeMessages = document.createTextNode(messages[i].parts[0].payload.content);
-                node.appendChild(writeMessages);
-                document.getElementById('messages').appendChild(node);
-            }
-        })
-        .catch(err => {
-            console.log( `Error fetching messages: ${err}`)
-            console.log(err)
-        })
+    if(chatManager !== null) {
+        chatManager.connect()
+            .then(loadedCurrentUser => {
+            currentUser = loadedCurrentUser;
+            // console.log(currentUser.rooms);
+            // console.log(currentUser.roomSubscriptions);
+            currentUser.subscribeToRoomMultipart({
+            roomId: currentUser.rooms[0].id,
+            hooks: {
+                onMessage: message => {
+                console.log("Received message:", message.parts[0].payload.content)
+                }
+            },
+            messageLimit: 0
+            }).then(response => {
+                fetchMessages(currentUser);
+            })
+            .catch(err => {
+                console.log("error");
+                console.log(err)
+            })
+        });
+    
+        function fetchMessages(currentUser) {
+            currentUser.fetchMultipartMessages({
+                roomId: ROOM_ID,
+                direction: 'older',
+                limit: 25,
+            })
+            .then(messages => {
+                for(var i = 0; i < messages.length; i++) {
+                    var node = document.createElement("LI")
+                    var writeMessages = document.createTextNode(messages[i].parts[0].payload.content);
+                    node.appendChild(writeMessages);
+                    document.getElementById('messages').appendChild(node);
+                }
+            })
+            .catch(err => {
+                console.log( `Error fetching messages: ${err}`)
+                console.log(err)
+            })
+        }
     }
 }
 
@@ -59,4 +62,21 @@ function hookUpChatKit() {
     return chatManager;
 }
 
-
+function sendMessageToRoom() {
+    var text = document.getElementById("message-text").value;
+    if( !text ) {
+        alert('please enter a message')
+    } else {
+        currentUser.sendSimpleMessage({
+            roomId: ROOM_ID,
+            text: text,
+          })
+          .then(messageId => {
+            console.log(`Added message to ${ROOM_ID}`)
+            window.location.reload()
+          })
+          .catch(err => {
+            console.log(`Error adding message to ${ROOM_ID}: ${err}`)
+          })
+    }
+}
